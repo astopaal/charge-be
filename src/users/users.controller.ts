a36f5385f05c24c7fe34a2fe655fe388490eb5e2
Samet Topal <astopaal@gmail.com>
2024-04-30 21:51:01 +0300

    user error handling

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,20 +3,37 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
+  HttpException,
+  HttpStatus,
+  Logger,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
+  logger = new Logger(UsersController.name);
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() user: User) {
-    return this.usersService.create(user);
+  async create(@Body() user: User) {
+    try {
+      await this.usersService.create(user);
+    } catch (error) {
+      this.logger.error(`${user.id} id numarali kullanici kaydedilirken bir hata olustu. Hata : ${error}`);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'Kullanici kaydedilirken bir hata olustu.',
+        },
+        HttpStatus.FORBIDDEN,
+        {
+          cause: error,
+        },
+      );
+    }
   }
 
   @Post('add-favorite')
@@ -24,35 +41,96 @@ export class UsersController {
     @Body('userId') userId: string,
     @Body('stationId') stationId: string,
   ) {
-    return this.usersService.addFavorite(userId, stationId);
+    try {
+      await this.usersService.addFavorite(userId, stationId);
+    } catch (error) {
+      this.logger.error(`${stationId} id numarali istasyon kaydedilirken bir hata olustu. Kullanici id : ${userId} Hata :  ${error}`);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'Favori istasyon kaydedilirken bir hata olustu.',
+        },
+        HttpStatus.FORBIDDEN,
+        {
+          cause: error,
+        },
+      );
+    }
   }
 
   @Get()
   async findAll() {
     try {
-      let users = await this.usersService.findAll();
-      console.log('====================================');
-      console.log(users);
-      console.log('====================================');
-      return users;
+      await this.usersService.findAll();
     } catch (error) {
-      console.error(error);
-      return [];
+      this.logger.error(`Kullanicilar getirilirken bir hata olustu. Hata : ${error}`);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'Kullanicilar getirilirken bir hata olustu.',
+        },
+        HttpStatus.FORBIDDEN,
+        {
+          cause: error,
+        },
+      );
     }
   }
 
   @Get(':id/favorites')
-  async getUserFavorites(@Param('id') userId: string): Promise<string[] | null> {
-    return this.usersService.getFavoritesByUserId(userId);
+  async getUserFavorites(@Param('id') userId: string) {
+    try {
+      await this.usersService.getFavoritesByUserId(userId);
+    } catch (error) {
+      this.logger.error(`${userId} id numarali kullanici favorileri getirilirken bir hata olustu. Hata : ${error}`);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'Kullanici favorileri getirilirken bir hata olustu.',
+        },
+        HttpStatus.FORBIDDEN,
+        {
+          cause: error,
+        },
+      );
+    }
   }
-  
+
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(id);
+  async findOne(@Param('id') userId: string) {
+    try {
+      await this.usersService.findOne(userId);
+    } catch (error) {
+      this.logger.error(`${userId} id numarali kullanici bilgileri getirilirken bir hata olustu. Hata : ${error}`);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'Kullanici getirilirken bir hata olustu.',
+        },
+        HttpStatus.FORBIDDEN,
+        {
+          cause: error,
+        },
+      );
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.deleteUserById(id);
+  async remove(@Param('id') userId: string) {
+    try {
+      await this.usersService.deleteUserById(userId);
+    } catch (error) {
+      this.logger.error(`${userId} id numarali kullanici silinirken bir hata olustu. Hata : ${error}`);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'Kullanici silinirken bir hata olustu.',
+        },
+        HttpStatus.FORBIDDEN,
+        {
+          cause: error,
+        },
+      );
+    }
   }
 }
